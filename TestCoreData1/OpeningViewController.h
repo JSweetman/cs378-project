43a5b6da26342848b57bd50c352bee26469ca393
Christopher Metcalf <csmetcalf@gmail.com>
2014-11-29 19:56:41 -0600

    Corrected user sign in issue with add

--- TestCoreData1/OpeningViewController.h
+++ TestCoreData1/OpeningViewController.h
@@ -9,5 +9,6 @@
 #import <UIKit/UIKit.h>
 
 @interface OpeningViewController : UIViewController
+- (IBAction)clickButtonAdd:(id)sender;
 
 @end
--- TestCoreData1/OpeningViewController.m
+++ TestCoreData1/OpeningViewController.m
@@ -8,6 +8,7 @@
 
 #import "OpeningViewController.h"
 #import "HappeningViewController.h"
+#import <Parse/Parse.h>
 
 
 @interface OpeningViewController ()
@@ -37,48 +38,7 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
--(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    
-    
-    if ([[segue identifier] isEqualToString:@"Happening"]){
-        NSLog(@"I'm happening\n");
-        HappeningViewController *vc = segue.destinationViewController;
-        //NSIndexPath *path = [self.tableView indexPathForCell:sender];
-      
-         NSCalendar *gregorian = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
-         NSDateComponents *components = [gregorian components: (NSHourCalendarUnit | NSMinuteCalendarUnit) fromDate: self];
-         NSInteger hour = [components hour];
-         NSInteger minute = [components minute];
- 
-        
-        NSDate *currentTime = [NSDate date];
-        NSDateFormatter *hourFormat = [[NSDateFormatter alloc] init];
-        [hourFormat setDateFormat:@"HH"];
-        NSDateFormatter *minuteFormat = [[NSDateFormatter alloc] init];
-        [minuteFormat setDateFormat:@"mm"];
-        NSString *theHour = [hourFormat stringFromDate:currentTime];
-        NSString *theMinute = [minuteFormat stringFromDate:currentTime];
-        NSLog(@"the hour is %@\n", theHour);
-        int valueHour = [theHour intValue];
-        int valueMinute = [theMinute intValue];
-        int numEvents = [self.DataModelList count];
-        for (int i = 0; i < numEvents; i++) {
-            DataModel *dm = [[DataModel alloc] init];
-            dm = _DataModelList[i];
-            NSString *str = dm.pickedTime;
-            str = [str substringToIndex: 2];
-            NSLog(@"the picked hour is %@\n", str);
-            if ([str isEqualToString: theHour])
-            {
-                [vc.nowList addObject:dm];
-            }
-            
-            
-        }
-    }
-}
-*/
+
 /*
 #pragma mark - Navigation
 
@@ -90,4 +50,15 @@
 }
 */
 
+- (IBAction)clickButtonAdd:(id)sender {
+    if (![PFUser currentUser])
+    {
+        [self performSegueWithIdentifier: @"SignIn" sender:self];
+    }
+    else
+    {
+        [self performSegueWithIdentifier: @"AddEvent" sender:self];
+    }
+        
+}
 @end
